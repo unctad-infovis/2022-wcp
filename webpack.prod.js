b3ae1fe542804404f56f8778a8d681320eb744b1
Teemo Tebest <teemo.tebest@gmail.com>
2022-06-29 17:20:56 +0200

    Updated production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,21 +11,7 @@ module.exports = merge(common, {
     minimize: true,
     minimizer: [
       new TerserPlugin({
-         terserOptions: {
-          ecma: undefined,
-          parse: {},
-          compress: {},
-          mangle: false, // Note `mangle.properties` is `false` by default.
-          module: false,
-          // Deprecated
-          output: null,
-          format: null,
-          toplevel: false,
-          nameCache: null,
-          ie8: false,
-          keep_classnames: undefined,
-          keep_fnames: false,
-          safari10: false,
+        terserOptions: {
         },
         test: /\.js(\?.*)?$/i
       }),
